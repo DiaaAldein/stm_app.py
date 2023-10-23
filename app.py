2a8c793c68b9caa23810c8f4f549eefb2989887f
DiaaAldein <120740089+DiaaAldein@users.noreply.github.com>
2023-10-23 11:41:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ df = pd.read_csv("advertising.csv")
 st.title("Click on Ads advertising ML Model")
 
 st.header('Data Info: ')
-st.info('''Problem Statement\n
+st.info('''
 Project prepared by: Diaa Aldein Alsayed Ibrahim Osman\n
 Prepared for: Epsilon AI Institute\n
+Problem Statement:\n
 We have a advertising dataset of a marketing agency. Goal is to develop a ML algorithm that predicts if a particular user will click on an advertisement. The dataset has 10 features:
 \n
 'Daily Time Spent on Site', 'Age', 'Area Income', 'Daily Internet Usage', 'Ad Topic Line', 'City', 'Male', 'Country', Timestamp' 'Clicked on Ad'.
